bc8619f4a188f19d8167dd3ee0b119f30cd2978b
Bernhard Richter <bernhard.richter@gmail.com>
2022-03-25 14:15:18 +0100

    Added xml docs

--- src/DbConfiguration/DbConfigurationExtensions.cs
+++ src/DbConfiguration/DbConfigurationExtensions.cs
@@ -3,29 +3,61 @@ using System.Data;
 
 namespace DbConfiguration
 {
+    /// <summary>
+    /// A set of extension methods to configure probider-specific implementations
+    /// of <see cref="IDbConnection"/>, <see cref="IDbCommand"/>, <see cref="IDataReader"/> and <see cref="IDbTransaction"/>.
+    /// </summary>
     public static class ConfigurationExtensions
     {
+        /// <summary>
+        /// Configures the underlying <see cref="IDbConnection"/>.
+        /// </summary>
+        /// <typeparam name="TConnection">The underlying <see cref="IDbConnection"/> implementation.</typeparam>
+        /// <param name="dbConnection">The target <see cref="IDbConnection"/>.</param>
+        /// <param name="configureConnection">The function to configure the underlying <see cref="IDbConnection"/>.</param>
+        /// <returns>This <paramref name="dbConnection"/> for chaining calls</returns>
         public static IDbConnection Configure<TConnection>(this IDbConnection dbConnection, Action<TConnection> configureConnection) where TConnection : IDbConnection
         {
             configureConnection(DbConfigurationOptions.GetInnerConnection<TConnection>(dbConnection));
             return dbConnection;
         }
 
+        /// <summary>
+        /// Configures the underlying <see cref="IDbCommand"/>.
+        /// </summary>
+        /// <typeparam name="TCommand">The underlying <see cref="IDbCommand"/> implementation.</typeparam>
+        /// <param name="dbCommand">The target <see cref="IDbCommand"/>.</param>
+        /// <param name="configureCommand">The function to configure the underlying <see cref="IDbCommand"/>.</param>
+        /// <returns>This <paramref name="dbCommand"/> for chaining calls</returns>
         public static IDbCommand Configure<TCommand>(this IDbCommand dbCommand, Action<TCommand> configureCommand) where TCommand : IDbCommand
         {
             configureCommand(DbConfigurationOptions.GetInnerCommand<TCommand>(dbCommand));
             return dbCommand;
         }
 
+        /// <summary>
+        /// Configures the underlying <see cref="IDataReader"/>.
+        /// </summary>
+        /// <typeparam name="TDataReader">The underlying <see cref="IDataReader"/> implementation.</typeparam>
+        /// <param name="dataReader">The target <see cref="IDataReader"/>.</param>
+        /// <param name="configureDataReader">The function to configure the underlying <see cref="IDataReader"/>.</param>
+        /// <returns>This <paramref name="dataReader"/> for chaining calls</returns>
         public static IDataReader Configure<TDataReader>(this IDataReader dataReader, Action<TDataReader> configureDataReader) where TDataReader : IDataReader
         {
             configureDataReader(DbConfigurationOptions.GetInnerDataReader<TDataReader>(dataReader));
             return dataReader;
         }
 
-        public static IDbTransaction Configure<TDbTransaction>(this IDbTransaction dbTransaction, Action<TDbTransaction> configureDataReader) where TDbTransaction : IDbTransaction
+        /// <summary>
+        /// Configures the underlying <see cref="IDbTransaction"/>.
+        /// </summary>
+        /// <typeparam name="TTransaction">The underlying <see cref="IDbTransaction"/> implementation.</typeparam>
+        /// <param name="dbTransaction">The target <see cref="IDbTransaction"/>.</param>
+        /// <param name="configureTransaction">The function to configure the underlying <see cref="IDbTransaction"/>.</param>
+        /// <returns>This <paramref name="dbTransaction"/> for chaining calls</returns>
+        public static IDbTransaction Configure<TTransaction>(this IDbTransaction dbTransaction, Action<TTransaction> configureTransaction) where TTransaction : IDbTransaction
         {
-            configureDataReader(DbConfigurationOptions.GetInnerDbTransaction<TDbTransaction>(dbTransaction));
+            configureTransaction(DbConfigurationOptions.GetInnerDbTransaction<TTransaction>(dbTransaction));
             return dbTransaction;
         }
     }
--- src/DbConfiguration/DbConfigurationOptions.cs
+++ src/DbConfiguration/DbConfigurationOptions.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Data;
 
+/// <summary>
+/// Configuration options for how to gain access to the underlying
+/// provider-specific types.
+/// </summary>
 public class DbConfigurationOptions
 {
     private static Func<IDbConnection, IDbConnection> _configureConnectionAccessor;
@@ -8,6 +12,9 @@ public class DbConfigurationOptions
     private static Func<IDataReader, IDataReader> _configureDataReaderAccessor;
     private static Func<IDbTransaction, IDbTransaction> _configureTransactionAccessor;
 
+    /// <summary>
+    /// Clears out all configuration.
+    /// </summary>
     public static void Clear()
     {
         _configureConnectionAccessor = (connection) => connection;
@@ -18,17 +25,42 @@ public class DbConfigurationOptions
 
     static DbConfigurationOptions() => Clear();
 
+    /// <summary>
+    /// Configures how to access the underlying <see cref="IDbConnection"/>.
+    /// </summary>
+    /// <typeparam name="TConnection">The type of <see cref="IDbConnection"/> for which to get the underlying/inner type.</typeparam>
+    /// <typeparam name="TInnerConnection">The underlying/inner <see cref="IDbConnection"/> implementing type.</typeparam>
+    /// <param name="configureAccessor">A function that configures how to access the underlying <see cref="IDbConnection"/>.</param>
     public static void ConfigureConnectionAccessor<TConnection, TInnerConnection>(Func<TConnection, TInnerConnection> configureAccessor) where TConnection : IDbConnection where TInnerConnection : IDbConnection
         => _configureConnectionAccessor = (connection) => configureAccessor((TConnection)connection);
 
+    /// <summary>
+    /// Configures how to access the underlying <see cref="IDbCommand"/>.
+    /// </summary>
+    /// <typeparam name="TCommand">The type of <see cref="IDbCommand"/> for which to get the underlying/inner type.</typeparam>
+    /// <typeparam name="TInnerCommand">The underlying/inner <see cref="IDbCommand"/> implementing type.</typeparam>
+    /// <param name="configureAccessor">A function that configures how to access the underlying <see cref="IDbCommand"/>.</param>
     public static void ConfigureCommandAccessor<TCommand, TInnerCommand>(Func<TCommand, TInnerCommand> configureAccessor) where TCommand : IDbCommand where TInnerCommand : IDbCommand
         => _configureCommandAccessor = (command) => configureAccessor((TCommand)command);
 
+
+    /// <summary>
+    /// Configures how to access the underlying <see cref="IDataReader"/>.
+    /// </summary>
+    /// <typeparam name="TDataReader">The type of <see cref="IDataReader"/> for which to get the underlying/inner type.</typeparam>
+    /// <typeparam name="TInnerDataReader">The underlying/inner <see cref="IDataReader"/> implementing type.</typeparam>
+    /// <param name="configureAccessor">A function that configures how to access the underlying <see cref="IDataReader"/>.</param>
     public static void ConfigureDataReaderAccessor<TDataReader, TInnerDataReader>(Func<TDataReader, TInnerDataReader> configureAccessor) where TDataReader : IDataReader where TInnerDataReader : IDataReader
         => _configureDataReaderAccessor = (dataReader) => configureAccessor((TDataReader)dataReader);
 
-    public static void ConfigureTransactionAccessor<TDbTransaction, TInnerDbTransaction>(Func<TDbTransaction, TInnerDbTransaction> configureAccessor) where TDbTransaction : IDbTransaction where TInnerDbTransaction : IDbTransaction
-        => _configureTransactionAccessor = (transaction) => configureAccessor((TDbTransaction)transaction);
+    /// <summary>
+    /// Configures how to access the underlying <see cref="IDbTransaction"/>.
+    /// </summary>
+    /// <typeparam name="TTransaction">The type of <see cref="IDbTransaction"/> for which to get the underlying/inner type.</typeparam>
+    /// <typeparam name="TInnerTransaction">The underlying/inner <see cref="IDbTransaction"/> implementing type.</typeparam>
+    /// <param name="configureAccessor">A function that configures how to access the underlying <see cref="IDbTransaction"/>.</param>
+    public static void ConfigureTransactionAccessor<TTransaction, TInnerTransaction>(Func<TTransaction, TInnerTransaction> configureAccessor) where TTransaction : IDbTransaction where TInnerTransaction : IDbTransaction
+        => _configureTransactionAccessor = (transaction) => configureAccessor((TTransaction)transaction);
 
     internal static TConnection GetInnerConnection<TConnection>(IDbConnection dbConnection) where TConnection : IDbConnection
     {
@@ -61,13 +93,13 @@ public class DbConfigurationOptions
         throw new InvalidOperationException($"Unable to resolve {typeof(TDataReader)} from {dataReader.GetType()}. Use 'DbConfigurationOptions.ConfigureDataReaderAccessor<{dataReader.GetType().Name}, {typeof(TDataReader).Name}>() to setup how to access {typeof(TDataReader).Name}'");
     }
 
-    internal static TDbTransaction GetInnerDbTransaction<TDbTransaction>(IDbTransaction dbTransaction) where TDbTransaction : IDbTransaction
+    internal static TTransaction GetInnerDbTransaction<TTransaction>(IDbTransaction dbTransaction) where TTransaction : IDbTransaction
     {
         var innerDataReader = _configureTransactionAccessor(dbTransaction);
-        if (innerDataReader is TDbTransaction reader)
+        if (innerDataReader is TTransaction reader)
         {
             return reader;
         }
-        throw new InvalidOperationException($"Unable to resolve {typeof(TDbTransaction)} from {dbTransaction.GetType()}. Use 'DbConfigurationOptions.ConfigureTransactionAccessor<{dbTransaction.GetType().Name}, {typeof(TDbTransaction).Name}>() to setup how to access {typeof(TDbTransaction).Name}'");
+        throw new InvalidOperationException($"Unable to resolve {typeof(TTransaction)} from {dbTransaction.GetType()}. Use 'DbConfigurationOptions.ConfigureTransactionAccessor<{dbTransaction.GetType().Name}, {typeof(TTransaction).Name}>() to setup how to access {typeof(TTransaction).Name}'");
     }
 }
